269e3870809c2cda416d8a8e3b723cf3cfd43d2d
GabrielDittrich <gdittrchcj@gmail.com>
2024-09-06 10:39:32 -0300

    Fazendo 3 Telas

--- Aula07/VariasTelas/app/src/main/java/tandera/hackerspace/variastelas/MainActivity.kt
+++ Aula07/VariasTelas/app/src/main/java/tandera/hackerspace/variastelas/MainActivity.kt
@@ -1,39 +1,28 @@
 package tandera.hackerspace.variastelas
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textInputNome : TextInputLayout
-    private lateinit var txtNome : TextInputEditText
-
-    private lateinit var  btn_cadastrar : Button
+    private lateinit var txtNome: TextInputEditText
+    private lateinit var btnCadastrar: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        textInputNome = findViewById(R.id.textInputNome)
-        txtNome = findViewById(R.id.txtNome)
+        txtNome = findViewById(R.id.textInputNome)
+        btnCadastrar = findViewById(R.id.btn_cadastro)
 
-        btn_cadastrar = findViewById(R.id.btn_cadastro)
-        btn_cadastrar.setOnClickListener {
+        btnCadastrar.setOnClickListener {
             val nome = txtNome.text.toString()
-
-            txtNome.setText("$nome")
+            val intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra("NOME_USUARIO", if (nome.isEmpty()) null else nome)
+            startActivity(intent)
         }
     }
 }
--- Aula07/VariasTelas/app/src/main/java/tandera/hackerspace/variastelas/MainActivity3.kt
+++ Aula07/VariasTelas/app/src/main/java/tandera/hackerspace/variastelas/MainActivity3.kt
@@ -1,32 +1,20 @@
 package tandera.hackerspace.variastelas
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main3)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val textView: TextView = findViewById(R.id.textView)
-
-        val btnVoltar: Button = findViewById(R.id.btn_voltar)
-
-        // Receber o dado extra do Intent
-        val nome = intent.getStringExtra("NOME_USUARIO")
-        textView.text = nome ?: "Nenhum nome recebido"
-
-        btnVoltar.setOnClickListener{
-            finish()
         }
     }
 }
