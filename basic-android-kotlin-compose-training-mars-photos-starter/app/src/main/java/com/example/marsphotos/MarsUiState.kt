e2c0792187c957576851fa91435b9ac8db709f59
GabrielDittrich <gdittrchcj@gmail.com>
2024-10-28 08:53:54 -0300

    Removendo arquivos desnecessários do repositório

--- basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/MarsUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.marsphotos
-
-sealed interface MarsUiState {
-    data class Success(val photos: String) : MarsUiState
-    object Error : MarsUiState
-    object Loading : MarsUiState
-}
